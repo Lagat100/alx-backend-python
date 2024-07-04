e352c3bdd8e918e38f848c10bb6d453badf1f837
Lagat100 <weldonkiprop1@gmail.com>
2024-07-04 22:25:24 +0000

    task 12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Task 12 module.
+'''
+
+
+from typing import Tuple, Any
+
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
+    zoomed_in = tuple(
+        item for item in lst
+        for i in range(factor)
+    )
+    return zoomed_in
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
+

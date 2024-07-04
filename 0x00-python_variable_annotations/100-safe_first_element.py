6b0f25556be15f435ca35b792602a5cb77336b64
Lagat100 <weldonkiprop1@gmail.com>
2024-07-04 22:36:41 +0000

    update fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,9 +4,9 @@
 
 from typing import Optional, Any
 
+
 def safe_first_element(lst: list) -> Optional[Any]:
     if lst:
         return lst[0]
     else:
         return None
-
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@
 
 from typing import Tuple, Any
 
+
 def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
     zoomed_in = tuple(
         item for item in lst
@@ -12,9 +13,9 @@ def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
     )
     return zoomed_in
 
+
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
-

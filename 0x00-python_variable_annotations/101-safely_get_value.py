1eda1c28053e547639e2d51462cc8e70a89547d7
Lagat100 <weldonkiprop1@gmail.com>
2024-07-04 22:22:52 +0000

    task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''Task 11 module.
+'''
+
+
+from typing import TypeVar, Dict, Optional
+
+# Define a type variable for the key
+K = TypeVar('K')
+
+# Define a type variable for the value
+V = TypeVar('V')
+
+def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> Optional[V]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+
